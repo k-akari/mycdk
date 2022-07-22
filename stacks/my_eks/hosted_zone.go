4d73afc465d23b5f233f02f644a88ed269351d84
k-akari <tennis.akari.abcdefg@gmail.com>
2022-07-22 11:47:53 +0000

    証明書を追加

--- stacks/my_eks/hosted_zone.go
+++ stacks/my_eks/hosted_zone.go
@@ -3,6 +3,7 @@ package my_eks
 import (
 	"os"
 
+	acm "github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
 	route53 "github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
 	constructs "github.com/aws/constructs-go/constructs/v10"
 	jsii "github.com/aws/jsii-runtime-go"
@@ -10,9 +11,14 @@ import (
 
 func NewHostZone(stack constructs.Construct) {
 	// パブリックホストゾーンを作成
-	route53.NewPublicHostedZone(stack, jsii.String("PublicHostedZone"), &route53.PublicHostedZoneProps{
+	hostedZone := route53.NewPublicHostedZone(stack, jsii.String("PublicHostedZone"), &route53.PublicHostedZoneProps{
 		ZoneName: jsii.String(os.Getenv("DOMAIN")),
 		Comment: jsii.String("free sample domain"),
 		CaaAmazon: jsii.Bool(true),
+	})
+
+	acm.NewCertificate(stack, jsii.String("Certificate"), &acm.CertificateProps{
+		DomainName: jsii.String("mycdk-app-example.tk"),
+		Validation: acm.CertificateValidation_FromDns(hostedZone),
 	})
 }
